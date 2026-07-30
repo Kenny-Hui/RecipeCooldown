af84ca033dc4cd5da59c8d69d72be36169b5b96b
AmberFrost <kenny.mh.hui@outlook.com>
2026-07-30 19:02:33 +0800

    whyyy jetbrain

--- src/main/java/com/lx862/recipecooldown/config/Config.java
+++ src/main/java/com/lx862/recipecooldown/config/Config.java
@@ -2,9 +2,10 @@ package com.lx862.recipecooldown.config;
 
 import folk.sisby.kaleido.api.ReflectiveConfig;
 import folk.sisby.kaleido.lib.quiltconfig.api.annotations.Comment;
+import folk.sisby.kaleido.lib.quiltconfig.api.annotations.DisplayName;
+import folk.sisby.kaleido.lib.quiltconfig.api.annotations.IntegerRange;
 import folk.sisby.kaleido.lib.quiltconfig.api.values.TrackedValue;
 import net.fabricmc.loader.api.FabricLoader;
-import org.jetbrains.annotations.Range;
 
 public class Config extends ReflectiveConfig {
     public static final Config INSTANCE = createToml(FabricLoader.getInstance().getConfigDir(), "", "recipecooldown", Config.class);
@@ -13,7 +14,8 @@ public class Config extends ReflectiveConfig {
     public final TrackedValue<Boolean> enabled = value(true);
 
     @Comment("The minimum cooldown required in millisecond for crafting recipe.")
-    @Range(from = 1, to = 10000)
+    @IntegerRange(min = 1, max = 10000)
+    @DisplayName("Cooldown period")
     public final TrackedValue<Long> cooldownMs = value(50L); // Base MSPT for server (20 TPS)
 
     public static void init() {
